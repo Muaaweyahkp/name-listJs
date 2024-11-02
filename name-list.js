ac21ae4b28d1fbf94daebb495e06164abe186b63
Muaaweyah Ibn Noufal K.P <muaaweyahkp@gmail.com>
2024-11-02 16:52:47 +0530

    name-list using js

---
+++ name-list.js
@@ -0,0 +1,22 @@
+// name
+var name1 ='muaaweyah kp';
+var name2 = 'Midlaj ';
+var name3 = 'Salman';
+var name4 = 'Najwa';
+
+// place
+var place1 = 'kozhikode';
+var place2 = 'kondotty' ;
+var place3 = 'vaidyarangadi';
+var place4 = 'malappuram';
+
+// age
+var age1 = 19;
+var age2 = 18;
+var age3 = 23;
+var age4 = 19;
+
+console.log( 'My name is '+name1+' and iam from '+place1+', iam '+age1+' years old.' )
+console.log( 'My name is '+name2+' and iam from '+place2+', iam '+age2+' years old.' )
+console.log( 'My name is '+name3+' and iam from '+place3+', iam '+age3+' years old.' )
+console.log( 'My name is '+name4+' and iam from '+place4+', iam '+age4+' years old.' )
